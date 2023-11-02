33a44a46139dd40dd4aae97c37681bd4c39e946b
Nithesh Shetty <nitheshe@in.ibm.com>
2023-11-02 18:44:21 +0530

    comments added

--- Paramiko.py
+++ Paramiko.py
@@ -1,3 +1,6 @@
+#program to ssh the remote server 
+#paramiko is library need for remote ssh but make sure it is installed at current location
+
 import paramiko 
 print("Using python3 to ssh to lremote server using paramiko ")
 client = paramiko.client.SSHClient()
