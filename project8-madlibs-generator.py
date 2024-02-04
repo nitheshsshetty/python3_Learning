e3e5b8a0325bd421f0fd2bb679a519abe7bfca03
Nithesh Shetty <nitheshe@in.ibm.com>
2024-02-04 18:12:18 +0530

    updates

---
+++ project8-madlibs-generator.py
@@ -0,0 +1,28 @@
+import string
+
+with open("project8-story.txt","r")as f:
+    story=f.read() 
+    print(story)
+target_start = "<"
+target_end = ">"
+start_of_word = -1
+words = set()
+for i ,char in enumerate(story):
+    if char == target_start :
+        start_of_word = i
+    if char == target_end and start_of_word != -1:
+        word = story[start_of_word:i+1]
+        words.add(word)
+        start_of_word = -1
+print(words)
+
+answers = {}
+for word in words:
+    answer = input(f"Enter a word for {word} : ")
+    answers[word]=answer
+print(answers)
+
+for word in words:
+    story=story.replace(word,answers[word])
+    
+print(story)
